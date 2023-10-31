a79501597f3c9ac9cf568906ea669d5c7341d990
Harsha Vardhan Gangineni <134440804+HarshaVardhanGangineniUs@users.noreply.github.com>
2023-10-30 21:35:42 -0400

    builder pattern implemented

---
+++ src/builder/Client.java
@@ -0,0 +1,13 @@
+package builder;
+
+import java.util.Map;
+
+public class Client {
+    public static void main(String[] args) {
+        HttpRequest request = new HttpRequest.Builder().method("GET")
+            .url("http://www.google.com")
+            .headers(Map.of("Accept", "text/html"))
+            .build();
+        System.out.println(request.url);
+    }
+}
---
+++ src/builder/HttpRequest.java
@@ -0,0 +1,36 @@
+package builder;
+
+import java.util.Map;
+
+public class HttpRequest {
+    String method;
+    String url;
+    Map<String, String> headers;
+    String acceptString;
+    
+    private HttpRequest() {}
+
+    public static class Builder {
+        private HttpRequest request = new HttpRequest();
+
+  
+        public Builder method(String method) {
+            request.method = method;
+            return this;
+        }
+        
+        public Builder url(String url) {
+            request.url = url;
+            return this;
+        }
+
+        public Builder headers(Map<String, String> headers) {
+            request.headers = headers;
+            return this;
+        }
+
+        public HttpRequest build() {
+            return request;
+        }
+    }
+}
